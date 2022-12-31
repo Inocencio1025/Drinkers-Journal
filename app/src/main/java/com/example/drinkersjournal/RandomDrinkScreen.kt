f35d33e33c5e42422d974d4386313c864daf1ea8
Rolando <100096333+Inocencio1025@users.noreply.github.com>
2022-12-31 16:06:51 -0500

    Still on random screen
    Tried to learn/implement constraint layouts in compose, but I feel that I'm slowing breaking things more and more so I gave up and use the code I wrote originally

--- app/src/main/java/com/example/drinkersjournal/RandomDrinkScreen.kt
+++ app/src/main/java/com/example/drinkersjournal/RandomDrinkScreen.kt
@@ -1,29 +1,22 @@
 package com.example.drinkersjournal
 
-import android.graphics.Bitmap
-import android.graphics.drawable.Drawable
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.navigation.NavController
-import com.bumptech.glide.Glide
+import androidx.constraintlayout.compose.layoutId
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import kotlinx.coroutines.CoroutineScope
@@ -31,15 +24,15 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okio.IOException
 import retrofit2.HttpException
-import java.io.InputStream
-import java.net.URL
+import retrofit2.Response
 
 
 
-var imageUrlStr = mutableStateOf("No Name")
-var drinkName = mutableStateOf("No Name")
-
-
+var imageUrlStr = mutableStateOf("Fetching drink...")
+var drinkName = mutableStateOf("")
+var instructions = mutableStateOf("")
+var ingredients: MutableList<String> by mutableStateOf(mutableListOf())
+var measurements: MutableList<String> by mutableStateOf(mutableListOf())
 
 
 
@@ -74,39 +67,174 @@ fun RandomDrinkScreen () {
 fun setContent() {
 
 
-    // Column Start
+    /*   Attempts to learn constrain layout
+    val constraints = ConstraintSet() {
+        val drinkImage = createRefFor("drinkImage")
+        val drinkName = createRefFor("drinkName")
+        val measurements = createRefFor("measurements")
+        val ingredients = createRefFor("ingredients")
+        val instructions = createRefFor("instructions")
+
+        constrain(drinkImage) {
+            top.linkTo(parent.top)
+            width = Dimension.fillToConstraints
+            height = Dimension.fillToConstraints
+        }
+        constrain(drinkName) {
+            top.linkTo(drinkImage.bottom)
+        }
+        constrain(measurements) {
+            top.linkTo(drinkName.bottom)
+            start.linkTo(parent.start)
+        }
+        constrain(ingredients) {
+            top.linkTo(drinkName.bottom)
+            end.linkTo(parent.end)
+
+        }
+        constrain(instructions) {
+            top.linkTo(ingredients.bottom)
+        }
+    }
+    
+    ConstraintLayout(constraints, modifier = Modifier
+        .fillMaxSize()
+        .verticalScroll(rememberScrollState())
+    ) {
+
+        // constraint layout contents
+        GlideImage(
+            model = imageUrlStr.value,
+            contentDescription = "Picture of Random Drink",
+            modifier = Modifier
+                .fillMaxWidth()
+                .layoutId("drinkImage")
+        )
+
+        CreateNameText(nameStr = drinkName.value)
+
+
+        // display ingredients text
+        CreateMeasurementText(measureStr = "measurements[0]")
+        CreateIngredientText(ingStr = "ingredients[0]")
+
+
+
+        // display instructions
+        CreateInstructionText(instrStr = instructions.value)
+
+
+
+
+    }
+
+
+     */
+
+
+    // renders background image
+    setBackgroundImage()
+    Spacer(modifier = Modifier.height(20.dp))
+
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 5.dp),
+            .padding(top = 5.dp)
+            .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+
+        // Start of content to display
+
+        // drink image
         GlideImage(
             model = imageUrlStr.value,
-            contentDescription = "Picture of Random Drink")
+            contentDescription = "Picture of Random Drink",
+            modifier = Modifier.fillMaxSize()
+        )
 
-        Text(
-            text = drinkName.value,
-            color = Color.White,
-            fontSize = 30.sp)
 
+
+
+        // drink name
+        CreateNameText(nameStr = drinkName.value)
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        // button for displaying the next drink
         Button(onClick = { retrieveRandomDrink() }) {
             Text(text = "Try Another")
         }
-    }
 
+        // display ingredients text
+        ingredients.forEach() {
+            CreateIngredientText(it)
+        }
+
+        // display instructions
+        CreateInstructionText(instrStr = instructions.value)
+
+
+    }
+}
+
+
+
+
+
+
+
+// Ingredient text
+@Composable
+fun CreateIngredientText(ingStr: String) {
+    // ingredient text
+    Text(
+        text = ingStr,
+        color = Color.White,
+        fontSize = 20.sp,
+        modifier = Modifier
+            .layoutId("ingredients")
+            .padding(8.dp))
 
 }
 
 @Composable
-fun setBackgroundImage(){
-    Image(
-        painter = painterResource(id = R.drawable.ic_temporary_home_background),
-        contentDescription = null,
-        modifier = Modifier.fillMaxSize(),
-        contentScale = ContentScale.FillBounds
+fun CreateMeasurementText(measureStr: String){
+    // measurement text
+    Text(
+        text = measureStr,
+        color = Color.White,
+        fontSize = 20.sp,
+        textAlign = TextAlign.Left,
+        modifier = Modifier.layoutId("measurements")
     )
+}
+
+
+@Composable
+fun CreateNameText(nameStr: String){
+    Text(
+        text = nameStr,
+        color = Color.White,
+        fontSize = 40.sp,
+        modifier = Modifier.layoutId("drinkName")
+    )
+}
+
+
+@Composable
+fun CreateInstructionText(instrStr: String){
+    Text(
+        text = instrStr,
+        color = Color.White,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(vertical = 36.dp, horizontal = 20.dp ))
 }
 
 
@@ -131,12 +259,112 @@ fun retrieveRandomDrink(){
             drinkName.value = response.body()!!.drinks[0].strDrink
             imageUrlStr.value = response.body()!!.drinks[0].strDrinkThumb
 
+
         }
 
+
+
+
+        //collects ingredients into private list in class
+        gatherIngredients(response)
+        gatherMeasurements(response)
+
+        instructions.value = response.body()!!.drinks[0].strInstructions
+
+
     }
-
-
 }
+
+
+fun gatherIngredients(response: Response<Drinks>){
+    ingredients.clear()
+
+    if(response.body()!!.drinks[0].strIngredient1 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient1)
+    if(response.body()!!.drinks[0].strIngredient2 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient2)
+    if(response.body()!!.drinks[0].strIngredient3 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient3)
+    if(response.body()!!.drinks[0].strIngredient4 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient4)
+    if(response.body()!!.drinks[0].strIngredient5 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient5)
+    if(response.body()!!.drinks[0].strIngredient6 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient6)
+    if(response.body()!!.drinks[0].strIngredient7 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient7)
+    if(response.body()!!.drinks[0].strIngredient8 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient8)
+    if(response.body()!!.drinks[0].strIngredient9 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient9)
+    if(response.body()!!.drinks[0].strIngredient10 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient10)
+    if(response.body()!!.drinks[0].strIngredient11 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient11)
+    if(response.body()!!.drinks[0].strIngredient12 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient12)
+    if(response.body()!!.drinks[0].strIngredient13 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient13)
+    if(response.body()!!.drinks[0].strIngredient14 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient14)
+    if(response.body()!!.drinks[0].strIngredient15 != null)
+        ingredients.add(response.body()!!.drinks[0].strIngredient15)
+}
+
+fun gatherMeasurements(response: Response<Drinks>) {
+    measurements.clear()
+
+    if(response.body()!!.drinks[0].strMeasure1 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure1)
+    if(response.body()!!.drinks[0].strMeasure2 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure2)
+    if(response.body()!!.drinks[0].strMeasure3 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure3)
+    if(response.body()!!.drinks[0].strMeasure4 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure4)
+    if(response.body()!!.drinks[0].strMeasure5 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure5)
+    if(response.body()!!.drinks[0].strMeasure6 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure6)
+    if(response.body()!!.drinks[0].strMeasure7 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure7)
+    if(response.body()!!.drinks[0].strMeasure8 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure8)
+    if(response.body()!!.drinks[0].strMeasure9 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure9)
+    if(response.body()!!.drinks[0].strMeasure10 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure10)
+    if(response.body()!!.drinks[0].strMeasure11 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure11)
+    if(response.body()!!.drinks[0].strMeasure12 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure12)
+    if(response.body()!!.drinks[0].strMeasure13 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure13)
+    if(response.body()!!.drinks[0].strMeasure14 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure14)
+    if(response.body()!!.drinks[0].strMeasure15 != null)
+        checkMeasurement(response.body()!!.drinks[0].strMeasure15)
+}
+
+// created because some measurements come in as unintended strings
+fun checkMeasurement(measure: String) {
+    if(!measure.equals(null))
+        measurements.add(measure)
+    else
+        measurements.add("")
+}
+
+@Composable
+fun setBackgroundImage(){
+    Image(
+        painter = painterResource(id = R.drawable.ic_temporary_home_background),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.FillBounds
+    )
+}
+
+
 
 
 
